be6a6849d8bfd9b726448334dcdb015272491a08
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:10:31 +0530

    Update Analyzing Employee Trends.sql

--- Analyzing Employee Trend/Analyzing Employee Trends.sql
+++ Analyzing Employee Trend/Analyzing Employee Trends.sql
@@ -8,3 +8,8 @@ SELECT department, COUNT(*) AS employee_count
 FROM hrdata
 GROUP BY department;
 
+--- 2.Calculate the average age for each department ---
+SELECT department, AVG(age) AS average_age
+FROM hrdata
+GROUP BY department;
+
