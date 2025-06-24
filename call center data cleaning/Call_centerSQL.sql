d6f3b3886890591011276782b48755566921cb77
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:24:21 +0530

    Update Call_centerSQL.sql

--- call center data cleaning/Call_centerSQL.sql
+++ call center data cleaning/Call_centerSQL.sql
@@ -7,3 +7,10 @@ SET SQL_SAFE_UPDATES = 0;
 --- changing date format---
 UPDATE call_center 
 SET call_timestamp = str_to_date(call_timestamp, "%m/%d/%Y");
+
+--- updating empty values to NUll ----
+UPDATE call_center 
+SET csat_score = NULL
+WHERE csat_score = '';
+SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM call_center LIMIT 10;
