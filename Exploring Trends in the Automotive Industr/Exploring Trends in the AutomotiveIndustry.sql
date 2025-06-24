4a746ae5d07123380343ca34aa091b6cd2ae28f7
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:17:23 +0530

    Update Exploring Trends in the AutomotiveIndustry.sql

--- Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
+++ Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
@@ -7,3 +7,11 @@ SELECT fuel, AVG(selling_price) AS avg_selling_price
 FROM car_info
 WHERE transmission = 'Manual' AND owner = 'First Owner'
 GROUP BY fuel;
+
+--- 2. Find the top 3 car models with the highest average mileage, considering only cars with more than 5 seats.---
+SELECT Name, AVG(mileage) AS avg_mileage
+FROM car_info
+WHERE seats > 5
+GROUP BY Name
+ORDER BY avg_mileage DESC
+LIMIT 3;
