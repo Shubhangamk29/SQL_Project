9b5110273aea71509cdeb4f1d60bb83128bfb590
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:17:46 +0530

    Update Exploring Trends in the AutomotiveIndustry.sql

--- Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
+++ Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
@@ -21,3 +21,10 @@ SELECT Name
 FROM car_info
 GROUP BY Name
 HAVING MAX(selling_price) - MIN(selling_price) > 10000;
+
+
+--- 4. Retrieve the names of cars with a selling price above the overall average selling price and a mileage below the overall average mileage. ---
+SELECT Name
+FROM car_info
+WHERE selling_price > (SELECT AVG(selling_price) FROM car_info)
+    AND mileage < (SELECT AVG(mileage) FROM car_info);
