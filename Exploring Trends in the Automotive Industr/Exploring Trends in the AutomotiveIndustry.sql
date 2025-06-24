a8bf60a101bb929a274760e41163ca08e80c4ca3
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:16:17 +0530

    Update Exploring Trends in the AutomotiveIndustry.sql

--- Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
+++ Exploring Trends in the Automotive Industr/Exploring Trends in the AutomotiveIndustry.sql
@@ -1,1 +1,9 @@
+use cars_info;
+--- To read Data ---
+SELECT * FROM car_info;
 
+--- 1. Calculate the average selling price for cars with a manual transmission and owned by the first owner, for each fuel type. ---
+SELECT fuel, AVG(selling_price) AS avg_selling_price
+FROM car_info
+WHERE transmission = 'Manual' AND owner = 'First Owner'
+GROUP BY fuel;
