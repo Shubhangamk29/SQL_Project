b875ebb9174831e58d0659cc3a93b0a8c7d014f8
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:24:10 +0530

    Update Call_centerSQL.sql

--- call center data cleaning/Call_centerSQL.sql
+++ call center data cleaning/Call_centerSQL.sql
@@ -3,3 +3,7 @@ SELECT * FROM call_center;
 
 ---- Data cleaning ---
 SET SQL_SAFE_UPDATES = 0;
+
+--- changing date format---
+UPDATE call_center 
+SET call_timestamp = str_to_date(call_timestamp, "%m/%d/%Y");
