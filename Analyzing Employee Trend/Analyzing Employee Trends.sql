85740ce480ee9dc462e532016696af299be32f93
Shubhangam Kumar <147134030+Shubhangamk29@users.noreply.github.com>
2025-06-24 14:10:17 +0530

    Update Analyzing Employee Trends.sql

--- Analyzing Employee Trend/Analyzing Employee Trends.sql
+++ Analyzing Employee Trend/Analyzing Employee Trends.sql
@@ -1,1 +1,10 @@
 
+use hr_data;
+--- To read Data ---
+SELECT * FROM hrdata;
+
+--- 1. Count the number of employees in each department---
+SELECT department, COUNT(*) AS employee_count
+FROM hrdata
+GROUP BY department;
+
